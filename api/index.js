6b8d93cbba9f1eaa6ee913c423091e6cf829e0f0
Abdullah22-22 <abdullahaltameemi28@gmail.com>
2025-08-11 11:42:31 +0300

    second-render

--- api/index.js
+++ api/index.js
@@ -7,30 +7,26 @@ import { startServer } from "./database.js";
 import roomRoutes from "./routes/Room.js";
 import authRoutes from "./routes/Auth.js";
 import hotelRoutes from "./routes/Hotel.js";
-import reservationRoutes from './routes/Reservation.js';
-import GetUsers from './routes/Auth.js';
+import reservationRoutes from "./routes/Reservation.js";
+import GetUsers from "./routes/Auth.js";
 import processPayment from "./routes/payment.js";
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 const app = express();
 
 // إنشاء مجلد "uploads" إذا لم يكن موجوداً
 if (!fs.existsSync("uploads")) {
-    fs.mkdirSync("uploads");
+  fs.mkdirSync("uploads");
 }
 
 // Middleware
 const corsOptions = {
-    origin: [
-    "http://localhost:3000",
-    "http://localhost:5173",
-    "https://travel-site-frontend-f85v.onrender.com" 
-],
+  origin: ["http://localhost:3000", "http://localhost:5173"],
 
-    methods: "GET, POST, PUT, DELETE",
-    credentials: true,
+  methods: "GET, POST, PUT, DELETE",
+  credentials: true,
 };
 app.use(cors(corsOptions));
 app.use(express.json());
